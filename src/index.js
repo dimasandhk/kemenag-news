1bd48b773bfe7c3134a34264803a40d9935aa3ed
Dimas Andhika Diputra <dimasandhikadiputra@gmail.com>
2021-08-06 21:54:46 +0700

    Redirect Unregistered Route

--- src/index.js
+++ src/index.js
@@ -6,4 +6,8 @@ const PORT = process.env.PORT || 3030;
 // API Main Route
 app.use("/api", require("./routers/main"));
 
+app.get(/.*/, (req, res) => {
+	res.redirect("/api/news");
+});
+
 app.listen(PORT, () => console.log(`Up on port ${PORT}`));
